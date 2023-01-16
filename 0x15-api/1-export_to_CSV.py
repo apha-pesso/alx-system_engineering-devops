c862b1ec38033c724c8ff1fbc03dfdfd81517d82
apha-pesso <mutiuayo99@gmail.com>
2023-01-16 16:55:07 +0000

    Convert data to csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module that Gather data from an API"""
+import requests
+from sys import argv
+from csv import writer, QUOTE_ALL
+
+if __name__ == "__main__":
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    task_list = []
+    for task in todos:
+        if (task.get('userId') == int(argv[1])):
+            task_list.append(task)
+
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    for user in users:
+        if (user.get("id") == int(argv[1])):
+            emp_name = user.get('username')
+
+    file_name = "{}.csv".format(argv[1])
+    with open(file_name, 'w', newline='', encoding='utf-8') as f:
+        writer = writer(f, quoting=QUOTE_ALL)
+
+        for task in task_list:
+            writer.writerow([argv[1], emp_name, task.get('completed'),
+                            task.get('title')])
